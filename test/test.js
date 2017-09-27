a437263707bba0227c9eef4c141d5729947c403b
hjiayz <hjiayz@163.com>
2017-09-27 09:25:24 +0800

    fix

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 let assert = require("assert");
 let t = new (require("../index.js"))(true)
+let type = t.type;
 let i = t.index;
 t.raw((o) => o === null).define("null");
 assert(i.null.is(null));
 t.raw((o) => typeof o == "function").define("func").define("ffunc");
 assert(i.func.is(() => 1));
-t.generic((o, p) => Array.isArray(o) && o.every((v) => p.is(v)), i.type).define("list").define("llist");
+t.generic((o, p) => Array.isArray(o) && o.every((v) => p.is(v)), type).define("list").define("llist");
 i.llist(i.func).define("flist").define("fflist");
 i.flist.assert([() => 1, () => 2]);
