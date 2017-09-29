29f6217e69ab49c4748447e52a5e3494d5a3dbe6
hjiayz <hjiayz@163.com>
2017-09-29 11:25:37 +0800

    eq

--- test/test.js
+++ test/test.js
@@ -4,8 +4,15 @@ let type = t.type;
 let i = t.index;
 t.raw((o) => o === null).define("null");
 assert(i.null.is(null));
-t.raw((o) => typeof o == "function").define("func").define("ffunc");
+let func = t.raw((o) => typeof o == "function")
+func.define("func").define("ffunc");
 assert(i.func.is(() => 1));
 t.generic((o, p) => Array.isArray(o) && o.every((v) => p.is(v)), type).define("list").define("llist");
-i.llist(i.func).define("flist").define("fflist");
+i.llist(i.func).define("fllist").define("ffllist");
+i.list(i.func).define("flist").define("fflist");
 i.flist.assert([() => 1, () => 2]);
+assert(i.null.eq(i.null))
+assert(i.flist.eq(i.fflist));
+assert(i.flist.eq(i.fllist));
+assert(i.func.eq(i.ffunc));
+assert(i.func.eq(func));
--- type.js
+++ type.js
@@ -1,9 +1,34 @@
-module.exports = class {
+let Type = module.exports = class {
     constructor(isdebug) {
         this.is = (obj) => false;
         this.assert = (obj, msg) => {
-            if ((isdebug) && (!this.is(obj))) throw new TypeError(msg || `not a ${this.name}`);
+            if ((isdebug) && (!this.is(obj))) throw new TypeError(msg || `not a ${this.meta.name}`);
             return this;
+        }
+        let igname = (o) => Object.assign({}, o, { name: "" })
+        let eq = (a, b) => {
+            if (typeof a != typeof b) return false;
+            if (typeof a == "object") {
+                if (a instanceof Type) {
+                    return a.eq(b);
+                }
+                if (Array.isArray(a)) {
+                    if (!Array.isArray(b)) return false;
+                    if (a.length != b.length) return false;
+                    return a.every((v, i) => eq(v, b[i]));
+                }
+                if (Object.keys(a).length != Object.keys(b).length) return false;
+                for (let i in a) {
+                    if (!eq(a[i], b[i])) return false;
+                }
+                return true;
+            }
+            if ((typeof a == "function") && (a.meta !== undefined) && (eq(igname(a.meta), igname(b.meta)))) return true
+            return a === b;
+        }
+        this.eq = (tp) => {
+            if (!(tp instanceof Type)) return false;
+            return eq(igname(this.meta), igname(tp.meta));
         }
     }
 }
